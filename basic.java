afad5a2d0241b8b6160d229f1918d4330cb9542c
Adhethya B <2484911@cognizant.com>
2026-03-02 14:35:34 +0530

    saved1

---
+++ basic.java
@@ -0,0 +1,18 @@
+class base{
+    int a = 10;
+
+}
+class sub extends base{
+    int a =20;
+
+
+    void display(){
+        a = 30;
+        System.out.print(a);
+    }
+}
+class drive{
+    sub s = new sub();
+    System.out.print(s.a);
+
+}
